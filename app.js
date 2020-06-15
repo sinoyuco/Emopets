7409b4bac4400327006c9ea2448ddf12da749a16
hira <hquddus021@gmail.com>
2020-06-15 10:00:53 -0400

    test

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
 const db = require('./config/keys').mongoURI;
 
+// this is a test
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
-    
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
