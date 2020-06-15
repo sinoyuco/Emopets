1661be65f72334fd11358b200bfb1878b53e4b17
omelnykov1 <sashqa.melnikov@gmail.com>
2020-06-15 16:05:15 -0400

    back

--- models/Like.js
+++ models/Like.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 const LikeSchema = new Schema({
     liker: {
         type: Schema.Types.ObjectId, 
-        ref: 'User'
+        ref: 'users'
     },
     liked: {
         type: Schema.Types.ObjectId, 
-        ref: 'User'
+        ref: 'users'
     }
 })
 
