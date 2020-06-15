3e45f22557f5504f7685de366795c214eaed5381
hira <hquddus021@gmail.com>
2020-06-15 09:26:56 -0400

    setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+app.get("/", (req, res) => res.send("Hello World"));
+const port = process.env.PORT || 5000;
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+    
+app.listen(port, () => console.log(`Server is running on port ${port}`));
