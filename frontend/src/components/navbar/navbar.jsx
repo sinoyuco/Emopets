cac25ff6eda9509aaecf124912ad754fa692d45e
steelydanrules <karl.konetsky@gmail.com>
2020-06-16 13:51:49 -0400

    some nav styling

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -6,15 +6,55 @@ class NavBar extends React.Component{
     super(props);
   }
 
+  notification(){
+    if (this.props.currentUser === undefined) {
+    return(
+      <>
+      </>
+    )
+  } else if (this.props.currentUser.likes === 0) {
+    return(
+      <div className="zero-notifications">
+        0
+      </div>
+    )
+  } else {
+    return (
+    <div className="some-notificaitons">
+      1
+    </div>
+    )
+  }
+
+}
 
   render(){
     return (
-      <div className="nav-bar">
-        <button className="navbar-back-to-home-button">
-          Home
-        </button>
-        <h6>hr</h6>
-      </div>
+      <nav className="nav-bar">
+        <Link to="/" style={{ marginRight: "33%" }}>
+          <img
+            src="http://localhost:3000/images/main_logo_no_text.png"
+            alt=""
+            height="60"
+            width="200"
+          ></img>
+        </Link>
+        <ul>
+          <li>
+            <Link to="/login">
+              <button className="navbar-button">Log In</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <button className="navbar-button">Home</button>
+            </Link>
+          </li>
+          <li>
+            {this.notification()}
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
