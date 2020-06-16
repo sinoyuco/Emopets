778d74be7252afb3300552cda5f92919f4b14696
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-06-16 09:03:13 -0400

    like express routing and model

---
+++ frontend/src/components/user/user_show_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import UserShow from './user_show';
+import User from '../../../../models/User';
+
+const mapStateToProps = (state) => {
+    return (null)
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return (null)
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(UserShow);
