41817184c969f5d8535b0b5a4c9759fa6d51fc1a
steelydanrules <karl.konetsky@gmail.com>
2020-06-15 23:08:02 -0400

    started splash.  need to add to css

--- frontend/src/components/splash/splash_page.jsx
+++ frontend/src/components/splash/splash_page.jsx
@@ -8,11 +8,20 @@ class SplashPage extends React.Component {
   }
 
   render() {
-    return(
-      <div>
-        SPLASH
+    return (
+      <div className="entire-splash">
+        <div className="top-of-splash">
+
+          <img
+            className="splash-logo"
+            src="http://localhost:3000/images/main_logo.png"
+            alt="My_Logo"
+            />
+        </div>
+
+        
       </div>
-    )
+    );
     
   }
 
