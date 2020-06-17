8f1286addbd1f9061bb0fcb70007ef272525a6fa
omelnykov1 <sashqa.melnikov@gmail.com>
2020-06-17 11:13:00 -0400

    finished filter and checkbox

--- frontend/src/components/play/play_form.jsx
+++ frontend/src/components/play/play_form.jsx
@@ -5,13 +5,20 @@ class Play extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            search: ''
+            search: []
         }
         this.updateCards = this.updateCards.bind(this);
     }
 
     updateCards(e) {
-        this.setState({search: e.target.value})
+        let box = Array.from(document.getElementsByClassName("language-option"))
+        let filtered = []
+        box.forEach(ele => {
+            if (ele.checked) {
+                filtered.push(ele.value)
+            }
+        })
+        this.setState({ search: filtered }) 
     }
 
 
@@ -20,18 +27,18 @@ class Play extends React.Component {
     }
 
     render() {
-        debugger
-        let usersSome
-        if (this.state.search.length) {
-            usersSome = this.props.users.filter(
-                user => (
-                    user.language === this.state.search
-                )
-            )
-        } else if (this.state.search.length === 0) {
-            usersSome = this.props.users
-        }
         if (this.props.users.length) {
+            let usersSome
+                if (this.state.search.length) {
+                    usersSome = this.props.users.filter(
+                        user => (
+                            this.state.search.includes(user.language)
+                        )
+                    )
+                } else if (this.state.search.some(ele => !ele.checked) || this.state.search.length === 0) {
+                    usersSome = this.props.users
+                }
+                debugger
             return(
                 <div className="cards-container">
                     <div className="card-container">
@@ -39,19 +46,19 @@ class Play extends React.Component {
                             usersSome.map(user => (
                                 <PlayItem 
                                     user={user}
-                                    key={user.id}
+                                    key={user._id}
                                 />
                             ))
                         }
                     </div>
                     <div className="language-options">
-                        <input type="checkbox" className="language-option" value='Ruby' onChange={this.updateCards}/>Ruby
-                        <input type="checkbox" className="language-option" value='Rails' onChange={this.updateCards}/>Rails
-                        <input type="checkbox" className="language-option" value='React' onChange={this.updateCards}/>React
-                        <input type="checkbox" className="language-option" value='Javascript' onChange={this.updateCards}/>Javascript
-                        <input type="checkbox" className="language-option" value='Node.js' onChange={this.updateCards}/>Node.js
-                        <input type="checkbox" className="language-option" value='HTML' onChange={this.updateCards}/>HTML
-                        <input type="checkbox" className="language-option" value='CSS' onChange={this.updateCards}/>CSS
+                        <label><input type="checkbox" id="check" className="language-option" value="Ruby" onChange={this.updateCards} />Ruby</label>
+                        <label><input type="checkbox" id="check" className="language-option" value="Rails" onChange={this.updateCards} />Rails</label>
+                        <label><input type="checkbox" id="check" className="language-option" value="React" onChange={this.updateCards} />React</label>
+                        <label><input type="checkbox" id="check" className="language-option" value="Javascript" onChange={this.updateCards} />Javascript</label >
+                        <label><input type="checkbox" id="check" className="language-option" value="Node.js" onChange={this.updateCards} />Node.js</label >
+                        <label><input type="checkbox" id="check" className="language-option" value="HTML" onChange={this.updateCards} />HTML</label >
+                        <label><input type="checkbox" id="check" className="language-option" value="CSS" onChange={this.updateCards} />CSS</label >
                     </div>
                 </div>
             )
