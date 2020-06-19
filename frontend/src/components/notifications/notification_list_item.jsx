aef9d4e981ea5e3c8d453d8f0decb16f328d21e6
omelnykov1 <sashqa.melnikov@gmail.com>
2020-06-18 23:52:44 -0400

    Update notification_list_item.jsx

--- frontend/src/components/notifications/notification_list_item.jsx
+++ frontend/src/components/notifications/notification_list_item.jsx
@@ -5,14 +5,9 @@ import React from 'react'
 
 export const NotificationListItem = (props) => {
   return(
-    <div className="notification-list-item-notif"
-    >
-      <img 
-      src={`/images/${props.users[props.notif.matched_with].language}-back-card.png`}
-      className="list-item-image"
-      />
+    <div className="notification-list-item-notif">
+      <img src={`/images/${props.users[props.notif.matched_with].language}-back-card.png`} className="list-item-image"/>
       <p style={{ margin: "15px" }}>You've matched with {props.users[props.notif.matched_with].name}</p>
-
       <p>email: {props.users[props.notif.matched_with].email}</p>
     </div>
   )
