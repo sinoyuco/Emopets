ce948d555a25c11534d6468b6724ef58ee652490
steelydanrules <karl.konetsky@gmail.com>
2020-06-18 20:17:45 -0400

    fixed splash working on team show

---
+++ frontend/src/components/team/team.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class TeamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return(
+      <div className="entire-team-show">
+
+        <div className="entire-team-grid">
+
+          <div className="entire-team-member">
+          </div>
+
+          <div className="entire-team-member">
+
+          </div>
+
+          <div className="entire-team-member">
+
+          </div>
+
+          <div className="entire-team-member">
+
+          </div>
+
+        </div>
+
+      </div>
+    )
+  }
+
+}
+
+
+
+export default TeamShow;
