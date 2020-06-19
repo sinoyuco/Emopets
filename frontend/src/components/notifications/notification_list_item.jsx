c0092dab751225d3136607c4297a6374215940a9
omelnykov1 <sashqa.melnikov@gmail.com>
2020-06-19 11:47:38 -0400

    added email

--- frontend/src/components/notifications/notification_list_item.jsx
+++ frontend/src/components/notifications/notification_list_item.jsx
@@ -5,12 +5,16 @@ import React from 'react'
 
 export const NotificationListItem = (props) => {
   debugger;
-  const newBanner = ((props.notif.type === 'seen') ? <div className="new-banner-container"><p className="new-banner-message">new</p></div> : null);
+  const newBanner = ((props.notif.type === 'unseen') ? <div className="new-banner-container"><p className="new-banner-message">new</p></div> : null);
   return(
     <div className="notification-list-item-notif">
       <img src={`/images/${props.users[props.notif.matched_with].language}-back-card.png`} className="list-item-image"/>
+      <div className="notif-body">
       <p style={{ margin: "15px" }}>You've matched with {props.users[props.notif.matched_with].name}</p>
-      <p style={{ margin: "0px" }}>email: {props.users[props.notif.matched_with].email}</p>
+      <p style={{ margin: "0px" }}>
+        <a className="send-email" href={`mailto:${props.users[props.notif.matched_with].email}`} target="_blank" rel="noopener noreferrer"><i className="far fa-envelope"></i></a>
+      </p>
+      </div>
       {newBanner}
     </div>
   )
