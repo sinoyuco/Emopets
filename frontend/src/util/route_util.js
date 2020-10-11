8debd53b678c42f8d36fbb54bd499b6feef0a9c5
omelnykov1 <sashqa.melnikov@gmail.com>
2020-10-11 16:49:39 -0400

    fixed authroute

--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
@@ -6,61 +5,30 @@ import { fetchUsers } from '../actions/user_actions'
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
     <Route path={path} exact={exact} render={(props) => (
-        !loggedIn ? (
-            <Component {...props} />
-        ) : (
-                <Redirect to="/show/" />
-            )
+        !loggedIn ? <Component {...props} /> : <Redirect to="/show/" /> 
     )} />
 );
 
-
 const Edit = ({ component: Component, path, users, currentUser, fetchUsers, exact }) => (
-    <Route path={path} exact={exact} render={(props) => {
-        //debugger
-        // fetchUsers();
-        //debugger; 
-        
-        return (
-            users[currentUser.id] !== currentUser ? (
-                    <Component {...props} />
-                
-            ) : (
-                    <Redirect to = "/show" />
-                  
-                )
-                
-                )
-            }} />
+    <Route path={path} exact={exact} render={(props) => (
+        users[currentUser.id] !== currentUser ? <Component {...props} /> : <Redirect to = "/show" />         
+    )} />
 );
 
 const Protected = ({ component: Component, loggedIn, ...rest }) => (
-    <Route
-        {...rest}
-        render={props =>
-            loggedIn ? (
-                <Component {...props} />
-            ) : (
-                    <Redirect to="/login" />
-                )
-        }
-    />
+    <Route {...rest} render={props => loggedIn ? <Component {...props} /> :  <Redirect to="/" />} />
 );
 
-const mapStateToProps = state => {
-    //debugger;
-    return ({ 
-        loggedIn: state.session.isAuthenticated,
-        currentUser: state.session.user,
-        users: state.users })
-};
+const mapStateToProps = state => ({ 
+    loggedIn: state.session.isAuthenticated,
+    currentUser: state.session.user,
+    users: state.users 
+})
 
-const mDTP = dispatch => {
-    return ({
-        fetchUsers: () => dispatch(fetchUsers())
-    })
-}
+const mDTP = dispatch => ({
+    fetchUsers: () => dispatch(fetchUsers())
+});
+
 export const AuthRoute = withRouter(connect(mapStateToProps, mDTP)(Auth));
 export const EditRoute = withRouter(connect(mapStateToProps, mDTP)(Edit));
-
 export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
